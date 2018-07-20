d24e9e625b17bf8f3e56985e24f0eae863fd936d
Fabricio C Zuardi <fabricio@fabricio.org>
2018-07-20 16:53:00 -0300

    prettier formatted and default commands tweaks

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const { Composer, log, session } = require('micro-bot')
+const { Composer, log, session } = require("micro-bot");
 
-const bot = new Composer()
+const bot = new Composer();
 
-bot.use(log())
-bot.use(session())
+bot.use(log());
+bot.use(session());
 
-bot.start(({ reply }) => reply('Welcome message'))
-bot.help(({ reply }) => reply('Help message'))
-bot.settings(({ reply }) => reply('Bot settings'))
+bot.start(({ reply }) =>
+  reply("Oi, eu sou o Lucas, um dos bots do Hackerspace Sanca.")
+);
+bot.help(({ reply }) =>
+  reply(
+    "Não tenho comandos úteis, ajude enviando um PR para https://github.com/sancahc/lucas-telegram"
+  )
+);
+bot.settings(({ reply }) => reply("Bot settings"));
 
-bot.command('date', ({ reply }) => reply(`Server time: ${Date()}`))
+bot.command("date", ({ reply }) => reply(`Server time: ${Date()}`));
 
-module.exports = bot
+module.exports = bot;
