a94b9af2f690db743e4bd267c4aef245323e77f2
Fabricio C Zuardi <fabricio@fabricio.org>
2018-07-20 22:06:59 -0300

    middleware to add know stickers "names" in the context state

---
+++ src/knownStickers.js
@@ -0,0 +1,18 @@
+const knownStickers = (ctx, next) => {
+  const { sticker } = ctx.message;
+  if (!sticker) {
+    return next();
+  }
+  const { set_name: setName, file_id: fileId } = sticker;
+
+  const ataIds = [
+    "CAADAQADNgIAAohO0QnRzmP_5sKnGgI",
+    "CAADAQADxwIAAohO0QkI2HtJ2G_L6gI"
+  ];
+  if (setName === "Monicaata" && ataIds.includes(fileId)) {
+    ctx.state.stickerMeaning = "ata";
+  }
+  return next();
+};
+
+module.exports = knownStickers;
